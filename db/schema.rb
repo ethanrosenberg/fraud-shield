21121e6f4fc1b264215130fb0218846c4c8b1cc9
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-09-12 21:36:46 -0700

    Added report path:string to reports table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_052936) do
+ActiveRecord::Schema.define(version: 2019_09_13_043544) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_052936) do
   create_table "reports", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "path"
   end
 
   create_table "urls", force: :cascade do |t|
